39f35259d4f8d5b20cacf10682be3fd54f0771c8
Ahnaf Ahmad <ahrmada95@gmail.com>
2022-07-13 11:55:40 -0400

    rendering maybe works

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const apiBaseUrl = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?';
 const urlCardName = `${apiBaseUrl}name=`;
 const urlCardId = `${apiBaseUrl}id=`;
 const baseUrlImg = 'https://ygoprodeck.com/pics/';
-const deckUrl = `http://localhost:3000/${currDeck}`;
+const decksUrl = 'http://localhost:3000/decks';
 
 //grab elements
 const cardViewImg = document.querySelector('#selected-card-img');
@@ -36,6 +36,7 @@ const cardViewName = document.querySelector('#select-card-name');
 const cardViewAttributes = document.querySelector('#select-card-attributes');
 const cardStats = document.querySelector('#select-card-stats');
 const cardEffects = document.querySelector('#select-card-effects');
+const deckForm = document.querySelector('#deck-form');
 
 //API CALL: fetch request to search by card's name
 const fetchByName = async (cardName) => {
@@ -59,14 +60,14 @@ const fetchById = async (cardId) => {
 
 //DB CALL: fetch list of deck na`mes
 const fetchListDecks = async () => {
-    let req = await fetch('http://localhost:3000/decks');
+    let req = await fetch(decksUrl);
     let res = await req.json();
     return Object.keys(res);
 }
 
 //DB CALLL: fetch deck information 
 const fetchDeck = async (deckName) => {
-    let req = await fetch(`http://localhost:3000/decks/`);
+    let req = await fetch(decksUrl);
     let res = await req.json();
     return res[`${deckName}`];
 }
@@ -94,16 +95,15 @@ const renderMainDeck = () => {
 
 //render extra deck
 const renderExtraDeck = () => {
-
+    console.log(currDeck);
 }
 
 //render side deck 
 const renderSideDeck = () => {
-
+    console.log(currDeck);
 }
 //render deck application portion
-const renderDeck = async (deckName) => {
-    await 
+const renderDeck = () => {
     renderMainDeck();
     renderExtraDeck();
     renderSideDeck();
@@ -128,175 +128,10 @@ const testCardView = async(cardNameOrId) => {
     cardEffects.textContent = cardSearched['desc']
 }
 
+deckForm.addEventListener('change', () => {
+    currDeck = deckForm[0].value;
+    renderDeck();
+})
+
 testCardView(80896940);
 populateDecks(initalizeDeck);
-
-// const newCardId = 99999999; 
-// const newName = 'Test Monster'; //card name
-// const newType = 'Monster'; //type of card
-// const newDesc = 'Testing constructor.'; //description of card or card effect
-// const newAtk = 2000; //attack point statline, NOTE that spells, traps do not have this stat
-// const newDef = 2000; //defense point statline, NOTE that spells, traps, links, do not have stat
-// const newLevel = 4; // card level (when applicable), NOTE that spells, traps, links, and XYZ do not have levels
-// const newRace = 'Warrior'; //race of card, for spell/trap this describes the type of spell/trap they are 
-// const newAttribute = 'Dark'; //attribute, does not apply to spell/trap
-// const newImageUrl = 'google.com';// image url to g
-
-// const newCard = new Card(newCardId, newName, newType, newDesc, newAtk, newDef, newLevel, newRace, newAttribute, newImageUrl);
-
-// console.log(newCard);
-
-// const cardViewer = document.querySelector('#card-viewer-img');
-// const cardViewerName = document.querySelector("#card-viewer > h1");
-// const cardViewerProperties = document.querySelector("#card-viewer > p.properties-text");
-// const cardViewerEffects = document.querySelector("#card-viewer > p.effect-text");
-// const cardViewerAtk = document.querySelector("#card-viewer > span > p:nth-child(1)");
-// const cardViewerDef = document.querySelector("#card-viewer > span > p:nth-child(2)");
-
-
-// const testCardView = async (cardNameorId) => {
-//     let cardTitle = document.createElement('h1');
-//     let cardImg = document.createElement('img');
-//     let cardDesc = document.createElement('p');
-//     cardDesc.classList.add('effect-text');
-//     //undefined var
-//     let cardSearched; 
-//     //check to see if it is a number or not
-//     if(isNaN(cardNameorId)){
-//         //not a number -> fetch by Name
-//         cardSearched = await fetchByName(cardNameorId);
-//     } else {
-//         //is a number -> fetch by ID
-//         cardSearched = await fetchByName(cardNameorId);
-//     }
-
-//     cardTitle.textContent = cardSearched['name'];
-//     //cardImg.src = 'https://storage.googleapis.com/ygoprodeck.com/pics/86066372.jpg'
-//     cardImg.src = `https://ygoprodeck.com/pics/${cardSearched['id']}.jpg`;
-    
-// // const cardViewerProperties = document.querySelector("#card-viewer > p.properties-text");
-// // const CardViewerEffects = document.querySelector("#card-viewer > p.effect-text");
-// // const CardViewerAtk = document.querySelector("#card-viewer > span > p:nth-child(1)");
-// // const CardViewerDef = document.querySelector("#card-viewer > span > p:nth-child(2)");
-// }
-
-// // const deckCount = 40;
-// // const exDeckCount = 15;
-
-// let mainDeck = document.getElementById('main-deck');
-// let extraDeck = document.getElementById('extra-deck');
-// let sideDeck = document.getElementById('side-deck');
-
-// // for (let a=0; a < deckCount; a++){
-// //     let tempImg = document.createElement('img');
-// //     tempImg.src='./assets/img/EHeroStratos.png';
-// //     tempImg.id= 'deck-builder-img';
-// //     mainDeck.append(tempImg);
-// //     tempImg.addEventListener('click', () => {
-// //         cardViewer.src = tempImg.src;
-// //     })
-// // }
-// // for (let b=0; b < exDeckCount; b++){
-// //     let tempImg = document.createElement('img');
-// //     tempImg.src='./assets/img/EHeroAbsZero.png';
-// //     tempImg.id= 'extra-deck-img';
-// //     extraDeck.append(tempImg);
-// //     tempImg.addEventListener('click', () => {
-// //         cardViewer.src = tempImg.src;
-// //     })    
-// // }
-// // for (let c=0; c < exDeckCount; c++){
-// //     let tempImg = document.createElement('img');
-// //     tempImg.src='./assets/img/RivalryOfWarlords.jpg';
-// //     tempImg.id= 'extra-deck-img';
-// //     sideDeck.append(tempImg);
-// //     tempImg.addEventListener('click', () => {
-// //         cardViewer.src = tempImg.src;
-// //     })
-// // }
-
-// const getDeckSource = async (url) => {
-//     let req = await fetch(url);
-//     let res = await req.json();
-//     return res;
-// }
-
-// const populateDeck = async (inputDeck) => {
-//     const deckSource = await inputDeck;
-
-//     let mainSize = 0;
-//     let extraSize = 0;
-//     let sideSize = 0;
-
-//     if (deckSource.sideDeck != null) {
-//         sideSize = deckSource.sideDeck.length;
-//         if (sideSize > 0) cardViewer.src = deckSource.sideDeck[0].imageUrl;
-//     }
-//     if (deckSource.extraDeck != null) {
-//         extraSize = deckSource.extraDeck.length;
-//         if (extraSize > 0) cardViewer.src = deckSource.extraDeck[0].imageUrl
-//     }
-//     if (deckSource.mainDeck != null) {
-//         mainSize = deckSource.mainDeck.length;
-//         if (mainSize > 0) cardViewer.src = deckSource.mainDeck[0].imageUrl
-//     }
-
-//     for (let a = 0; a < mainSize; a++) {
-//         let tempImg = document.createElement('img');
-//         tempImg.src = deckSource.mainDeck[a].imageUrl;
-//         tempImg.classList.add('deck-builder-img');
-//         mainDeck.append(tempImg);
-//         let click_shit = tempImg.addEventListener('click', () => {
-//             cardViewer.src = tempImg.src;
-//             cardViewerName.textContent = deckSource.mainDeck[a]['name'];
-//             cardViewerProperties.textContent = `${deckSource.mainDeck[a]['race']}/${deckSource.mainDeck[a]['type']}`;
-//             cardViewerEffects.textContent = `${deckSource.mainDeck[a]['desc']}`;
-//             if(deckSource.mainDeck[a]['type'] != "Spell Card" && deckSource.mainDeck[a]['type'] != "Trap Card") {
-//                 cardViewerAtk.textContent = `ATK/${deckSource.mainDeck[a]['atk']}`;
-//                 if(deckSource.mainDeck[a]['type']!='Link Monster') {
-//                     cardViewerDef.textContent = `DEF/${deckSource.mainDeck[a]['def']}`;
-//                 }
-//             }
-//         })
-//         tempImg.addEventListener('contextmenu', event => {
-//             event.preventDefault();
-//             tempImg.remove();
-//             tempImg.removeEventListener(click_shit);
-//         })
-//     }
-//     for (let b = 0; b < extraSize; b++) {
-//         let tempImg = document.createElement('img');
-//         tempImg.src = deckSource.extraDeck[b].imageUrl;
-//         tempImg.classList.add('extra-deck-img')
-//         extraDeck.append(tempImg);
-//         tempImg.addEventListener('click', () => {
-//             cardViewer.src = tempImg.src;
-//         })
-//     }
-//     for (let c = 0; c < sideSize; c++) {
-//         let tempImg = document.createElement('img');
-//         tempImg.src = deckSource.sideDeck[c].imageUrl;
-//         tempImg.classList.add('extra-deck-img')
-//         sideDeck.append(tempImg);
-//         tempImg.addEventListener('click', () => {
-//             cardViewer.src = tempImg.src;
-//         })
-//     }
-// }
-
-
-// const clearDecks = () => {
-//     mainDeck.innerHTML = ''
-//     extraDeck.innerHTML = ''
-//     sideDeck.innerHTML = ''
-// }
-
-// const deckForm = document.getElementById("deck-form")
-
-// populateDeck(getDeckSource(deckForm.decks.value))
-
-
-// deckForm.addEventListener('change', () => {
-//     clearDecks()
-//     populateDeck(getDeckSource(deckForm.decks.value))
-// })
